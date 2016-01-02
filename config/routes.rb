c272d404e0c3915cac9e3f0eb6d578fcd67d5a63
mardiniii <sezama03@hotmail.com>
2016-01-02 14:14:53 -0500

    Define root page and fix route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  get 'pages/home'
-  get 'pages/trading_apps'
-  get 'pages/how_it_works?'
-  get 'pages/faq'
+  root "pages#home"
+  get 'pages/home' => 'pages#home'
+  get 'pages/trading_apps' => 'pages#trading_apps'
+  get 'pages/how_it_works?' => 'pages#how_it_works?'
+  get 'pages/faq' => 'pages#faq'
 end

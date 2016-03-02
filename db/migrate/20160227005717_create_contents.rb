b93b220306b6c04c06b85a644d4afee27c5b438a
mardiniii <sezama03@hotmail.com>
2016-03-02 11:06:04 -0500

    Add feature to control copies into Home, Contact and About Us Section

--- db/migrate/20160227005717_create_contents.rb
+++ db/migrate/20160227005717_create_contents.rb
@@ -14,22 +14,35 @@ class CreateContents < ActiveRecord::Migration
       t.string :carousel_link_3
       t.string :carousel_link_3_text
       t.string :feature_1_title
-      t.string :feature_1_text
+      t.text :feature_1_text
       t.string :feature_2_title
-      t.string :feature_2_text
+      t.text :feature_2_text
       t.string :feature_3_title
-      t.string :feature_3_text
+      t.text :feature_3_text
       t.string :feature_4_title
-      t.string :feature_4_text
+      t.text :feature_4_text
       t.string :section_1_title
-      t.string :section_1_first_paragraph
-      t.string :section_1_second_paragraph
+      t.text :section_1_first_paragraph
+      t.text :section_1_second_paragraph
       t.string :section_2_title
-      t.string :section_2_first_paragraph
-      t.string :section_2_second_paragraph
+      t.text :section_2_first_paragraph
+      t.text :section_2_second_paragraph
       t.string :section_3_title
-      t.string :section_3_first_paragraph
-      t.string :section_3_second_paragraph
+      t.text :section_3_first_paragraph
+      t.text :section_3_second_paragraph
+      t.text :about_us_first_paragraph
+      t.text :about_us_second_paragraph
+      t.text :about_us_third_paragraph
+      t.string :about_us_1_title
+      t.text :about_us_1_first_paragraph
+      t.text :about_us_1_second_paragraph
+      t.string :about_us_2_title
+      t.text :about_us_2_first_paragraph
+      t.text :about_us_2_second_paragraph
+      t.string :about_us_3_title
+      t.text :about_us_3_first_paragraph
+      t.text :about_us_3_second_paragraph
+      t.text :contact_us_paragraph
 
       t.timestamps null: false
     end

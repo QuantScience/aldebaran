6adff728c8b39d75b8c2ad1416862c5bc45d9a8d
mardiniii <sezama03@hotmail.com>
2016-03-08 16:56:27 -0500

    Run paperclip migration for image

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,4 +9,6 @@
 #
 
 class Image < ActiveRecord::Base
+  has_attached_file :images, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :images, content_type: /\Aimage\/.*\Z/
 end

bc8d4679221c230e468173537ebd720b8f6494d0
mardiniii <sezama03@hotmail.com>
2016-03-08 17:07:12 -0500

    Add enum features to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,13 +2,20 @@
 #
 # Table name: images
 #
-#  id         :integer          not null, primary key
-#  location   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  location           :string
+#  image_type         :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Image < ActiveRecord::Base
   has_attached_file :images, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :images, content_type: /\Aimage\/.*\Z/
+
+  enum step_type: [:carousel_1, :section, :carousel_2]
 end

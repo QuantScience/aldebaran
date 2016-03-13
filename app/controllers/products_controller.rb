3a30dd8e609b0e4c0a2e35cdeb4b69aea7cbbb57
mardiniii <sezama03@hotmail.com>
2016-03-13 12:34:11 -0500

    Add edit and update action to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,9 +18,18 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "The Product was edited successfully"
+      redirect_to products_path
+    else
+      flash[:alert] = 'There has been an error editing the Product'
+      render 'edit'
+    end
   end
 
   def destroy

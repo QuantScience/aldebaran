add9274793b025e1e3d93da7e35e5bee7acfec63
mardiniii <sezama03@hotmail.com>
2016-03-23 12:07:09 -0500

    Add size to section images

--- app/models/section_image.rb
+++ app/models/section_image.rb
@@ -11,7 +11,7 @@
 class SectionImage < ActiveRecord::Base
   belongs_to :product
 
-  has_attached_file :image, styles: { section_size: "750x500#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
+  has_attached_file :image, styles: { section_size: "750x500#", thumb: "150x150#", mini_thumb: "75x75#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :image
 end

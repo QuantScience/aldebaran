a3961e798969aa3e26c8fb48293e2acd88738dc2
mardiniii <sezama03@hotmail.com>
2016-03-25 15:47:58 -0500

    Add destroy action and its admin filter to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :its_admin?, :only => [:index,:destroy]
 
   def new
     @product = Product.find(params[:product_id])
@@ -22,14 +23,30 @@ class OrdersController < ApplicationController
     end
   end
 
+  def index
+    @orders = Order.includes(:product, :user)
+  end
+
   def show
     @order = Order.find(params[:id])
     @product = @order.product
     @product_images = @product.product_images
   end
 
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to orders_path
+  end
+
   private
     def order_params
       params.require(:order).permit(:trading_software, :customer_id, :accepts_disclaimer)
+    end
+
+    def its_admin?
+      unless current_user.admin?
+        redirect_to root_path, :alert => "You don't have access to this route"
+      end
     end
 end

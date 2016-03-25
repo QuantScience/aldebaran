1ffe8789f9a63b5af9d9ec3f4e4a838e406aceba
mardiniii <sezama03@hotmail.com>
2016-03-25 12:23:51 -0500

    Add strong parameters to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,9 @@ class OrdersController < ApplicationController
     @product_images = @product.product_images
     @order = Order.new
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:trading_software, :customer_id, :accepts_disclaimer)
+    end
 end

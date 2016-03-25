0928894e0f2e9462e10a87c5282d7ef8fb25b4b2
mardiniii <sezama03@hotmail.com>
2016-03-25 12:06:10 -0500

    Start to define new order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,7 @@
 class OrdersController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @product_images = @product.product_images
+    @order = Order.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   resources :steps
   resources :images
   resources :products
+  resources :orders
 end

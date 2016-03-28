08c799c07fc51e4fc04fbad8178bee562c9173d7
mardiniii <sezama03@hotmail.com>
2016-03-28 12:59:44 -0500

    Add url for production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Production URL and mailer
+  config.action_mailer.default_url_options = { host: 'http://www.aldebarantech.com/' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 

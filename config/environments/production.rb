18c129ae7e63aa3c1f74942147675770175375a1
mardiniii <sezama03@hotmail.com>
2016-03-28 13:14:54 -0500

    Add mailer set up for production envirooment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,17 @@ Rails.application.configure do
   # Production URL and mailer
   config.action_mailer.default_url_options = { host: 'http://www.aldebarantech.com/' }
 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
